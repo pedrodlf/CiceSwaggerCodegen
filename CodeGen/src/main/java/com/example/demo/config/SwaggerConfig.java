c9e5001ce0253cb04bc115cbc8b07512047b5e54
Unknown <pedrodlafuente@gmail.com>
2017-11-29 15:39:51 +0100

    do it

---
+++ CodeGen/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+//	@Bean
+//    public Docket api() { 
+//        return new Docket(DocumentationType.SWAGGER_2)  
+//          .select()                                  
+//          .apis(RequestHandlerSelectors.any())              
+//          .paths(PathSelectors.any())                          
+//          .build();                                           
+//    }
+	   @Bean
+	    public Docket customImplementation(){
+	    	
+	    	
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()
+	                		
+	                    .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+
+	                
+	                    .build()
+
+	                .apiInfo(apiInfo());
+
+	    }
+	   
+	    ApiInfo apiInfo() {
+	        return new ApiInfoBuilder()
+	            .title("CICE COdegen")
+	            .description("Api de muestra")
+	            .license("MIT")
+	            .licenseUrl("http://opensource.org/licenses/MIT")
+	            //.termsOfServiceUrl("http://swagger.io")
+	            .version("1.0")
+	            .contact(new Contact("","", "foo@example.com"))
+	            .build();
+	    }
+	
+}
