c7ecbaae42760380561eda20c44c83ef32bf516f
Unknown <pedrodlafuente@gmail.com>
2017-11-29 12:04:51 +0100

    Create Project

---
+++ CodeGen/src/main/java/com/example/demo/CodeGenApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeGenApplication.class, args);
+	}
+}
